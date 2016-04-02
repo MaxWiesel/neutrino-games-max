728930ecb78b875fb8feb2ac95e470fc5b56c0ea
Jacek Jendrzej <crashdvb@googlemail.com>
2016-04-02 20:23:53 +0200

    tank: Code modifications for cst hardware

--- tank/somain.c
+++ tank/somain.c
@@ -45,11 +45,14 @@ int main( )
 	int				x;
 	int fdfb = -1, fdrc = -1;
 
-	if ( FBInitialize( 720, 576, 8, fdfb ) < 0 )
+	const int dx = 1280;
+	const int dy = 720;
+
+	if ( FBInitialize( dx, dy, 8, fdfb ) < 0 )
 		return -1;
 
 	setup_colors();
-	FBFillRect( 0, 0, 1400, 800, BNR0 );
+	FBFillRect( 0, 0, dx, dy, BNR0 );
 
 	if ( RcInitialize( fdrc ) < 0 )
 		return -1;
